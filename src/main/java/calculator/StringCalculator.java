5543ed91f47b7afab1e69e96e481676392380703
yunyoung1819 <yunyoung1819@hanmail.net>
2020-04-14 23:42:37 +0900

    chapter2 문자열 계산기 리팩토링-2020.04.15

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,10 +2,34 @@ package calculator;
 
 public class StringCalculator {
     public int add(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
         }
 
-        return Integer.parseInt(text);
+        return sum(toInts(split(text)));
+    }
+
+    private boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private String[] split(String text) {
+        return text.split(",|:");
+    }
+
+    private int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+
+    private int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
--- src/main/java/test/StringCalculatorTest.java
+++ src/main/java/test/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import calculator.StringCalculator;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
@@ -23,5 +24,21 @@ public class StringCalculatorTest {
     @Test
     public void add_숫자하나() throws Exception {
         assertEquals(1, cal.add("1"));
+    }
+
+    @Test
+    public void add_쉼표구분자() throws Exception {
+        assertEquals(3, cal.add("1,2"));
+    }
+
+    @Test
+    public void split() {
+        assertArrayEquals(new String[] {"1"}, "1".split(","));
+        assertArrayEquals(new String[] {"1", "2"}, "1,2".split(","));
+    }
+
+    @Test
+    public void add_쉼표_또는_콜론_구분자() throws Exception {
+        assertEquals(6, cal.add("1,2:3"));
     }
 }
